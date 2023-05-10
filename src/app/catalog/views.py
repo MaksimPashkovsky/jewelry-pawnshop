d18abd1128becd51500939ff42c068e77e4367cb
MaksimPashkovsky <maksim_pashkovskiy@mail.ru>
2023-05-10 23:50:26 +0300

    sorting and filters

--- src/app/catalog/views.py
+++ src/app/catalog/views.py
@@ -11,37 +11,96 @@ def catalog_page():
     for a_type in ['ring', 'bracelet', 'watch', 'necklace', 'pendant', 'chain', 'earrings', 'brooch']:
         session['match-' + a_type] = ''
 
-    if request.method == 'GET':
-        articles_per_page = 12
-        page = request.args.get('page', 1, type=int)
-        offset = articles_per_page * (page - 1)
-        articles = storage.get_articles_for_sale(offset, articles_per_page)
-        total_pages = math.ceil(storage.get_articles_for_sale_count() / articles_per_page)
-        print('total_pages = ', total_pages, file=sys.stderr)
-        return render_template('catalog.html', articles=sorted(articles, key=attrgetter('name')), current_page=page,
-                               total_pages=total_pages)
+    for sort in ['name-asc', 'name-desc', 'estimated_price-asc', 'estimated_price-desc']:
+        session['sort-' + sort] = ''
 
-    articles = list()
+    session['search-string'] = ''
 
-    for a_type in ['ring', 'bracelet', 'watch', 'necklace', 'pendant', 'chain', 'earrings', 'brooch']:
-        if request.form.get('match-' + a_type):
-            articles.extend(storage.get_article_type_by_name(a_type.capitalize()).articles)
-            session['match-' + a_type] = 'checked'
+    session['price-start'] = ''
+    session['price-end'] = ''
 
-    price_start = request.form.get('price-start')
-    price_end = request.form.get('price-end')
+    sorting_option = ""
 
-    filtered_articles = list(filter(lambda x: float(price_start) <= x.estimated_price <= float(price_end), articles))
+    if request.method == "POST":
+        sorting_option = request.form.get('sorting-input')
+        session['sort-' + sorting_option] = 'selected'
 
-    # sorting_option = request.form.get('sorting')
+    if request.args.get('sort'):
+        sorting_option = request.args.get('sort')
 
-    # if sorting_option == 'num-of-purchased':
-    #    sorted_articles = sorted(filtered_articles, key=lambda x: len(x.users_have_in_history), reverse=True)
-    # else:
-    #    field, order = sorting_option.split('-')
-    #    sorted_articles = sorted(filtered_articles, key=attrgetter(field), reverse=order == 'desc')
+    if not sorting_option:
+        sorting_option = "name-asc"
 
-    return render_template('catalog.html', articles=filtered_articles)
+    field, order = sorting_option.split('-')
+
+    articles_per_page = 1
+    page = request.args.get('page', 1, type=int)
+    offset = articles_per_page * (page - 1)
+
+    filtered_types = []
+
+    if request.method == 'POST':
+        for a_type in [t.name.lower() for t in storage.get_all_article_types()]:
+            if request.form.get('match-{}'.format(a_type)):
+                session['match-{}'.format(a_type)] = 'checked'
+                filtered_types.append(a_type.capitalize())
+
+    if request.args.get('types'):
+        filtered_types = request.args.get('types').split(',')
+
+    if not filtered_types:
+        filtered_types = [t.name for t in storage.get_all_article_types()]
+
+    search_string = ''
+
+    if request.method == 'POST':
+        search_string = request.form.get('search-string')
+        session['search-string'] = search_string
+
+    if request.args.get('search'):
+        search_string = request.args.get('search')
+
+    price_start, price_end = 0, 0
+
+    if request.method == 'POST':
+        price_start = request.form.get('price-start')
+        price_end = request.form.get('price-end')
+        session['price-start'] = price_start
+        session['price-end'] = price_end
+
+    if request.args.get('price-start'):
+        price_start = request.args.get('price-start')
+
+    if request.args.get('price-end'):
+        price_end = request.args.get('price-end')
+
+    if not price_start:
+        price_start = storage.get_articles_for_sale_min_price(filtered_types, search_string)
+        session['price-start'] = price_start
+        print('price_start = ', price_start, file=sys.stderr)
+
+    if not price_end:
+        price_end = storage.get_articles_for_sale_max_price(filtered_types, search_string)
+        session['price-end'] = price_end
+        print('price_end = ', price_end, file=sys.stderr)
+
+    def update_query_param(name, value):
+        base, *params = request.url.split('?')
+        params = dict(el.split('=') for el in params[0].split('&')) if params else dict()
+        params[name] = value
+        params['types'] = ",".join(filtered_types)
+        params['sort'] = sorting_option
+        params['search'] = search_string
+        params['price-start'] = price_start
+        params['price-end'] = price_end
+        return base + '?' + "&".join(f"{p}={v}" for p, v in params.items())
+
+    articles_on_page = storage.get_articles_for_sale(offset, articles_per_page, field, 1 if order == 'desc' else 0, filtered_types, search_string, price_start, price_end)
+
+    total_pages = math.ceil(storage.get_articles_for_sale_count(filtered_types, search_string, price_start, price_end) / articles_per_page)
+
+    return render_template('catalog.html', articles=articles_on_page, current_page=page,
+                           total_pages=total_pages, update_query_param=update_query_param)
 
 
 @catalog.route('/article/<int:id>')
--- src/app/database_service.py
+++ src/app/database_service.py
@@ -1,5 +1,10 @@
+import sys
+from operator import attrgetter
+
 from app.models import *
 from app.db_setup import session
+from sqlalchemy import or_
+from sqlalchemy import func
 
 
 class DatabaseService:
@@ -13,11 +18,85 @@ class DatabaseService:
     def get_all_articles_for_sale(self):
         return self.session.query(Article).filter_by(for_sale=True).all()
 
-    def get_articles_for_sale(self, offset, limit):
-        return self.session.query(Article).filter_by(for_sale=True).order_by(Article.name).offset(offset).limit(limit).all()
+    def get_articles_for_sale(self, offset: int, limit: int, order_by: str, desc: bool, types: list, search_string: str, min_price: float, max_price: float):
 
-    def get_articles_for_sale_count(self):
-        return self.session.query(Article).filter_by(for_sale=True).count()
+        query = self.session.query(Article) \
+            .filter(Article.for_sale == True)
+
+        inner = ", ".join(f"Article.type.has(name='{t}')" for t in types)
+        expr = f"query = query.filter(or_({inner}))"
+        print(expr)
+
+        lcls = locals()
+        exec(expr, globals(), lcls)
+        query = lcls['query']
+
+        query = query.filter(Article.estimated_price.between(min_price, max_price))
+        print(query)
+
+        if desc:
+            query = query.order_by(getattr(Article, order_by).desc())
+        else:
+            query = query.order_by(getattr(Article, order_by))
+
+        if search_string:
+            query = query.filter(Article.name.ilike(f"%{search_string}%"))
+
+        return query.offset(offset) \
+            .limit(limit) \
+            .all()
+
+    def get_articles_for_sale_count(self, types: list, search_string: str, min_price: float, max_price: float):
+        query = self.session.query(Article) \
+            .filter(Article.for_sale == True)
+
+        inner = ", ".join(f"Article.type.has(name='{t}')" for t in types)
+        expr = f"query = query.filter(or_({inner}))"
+
+        lcls = locals()
+        exec(expr, globals(), lcls)
+        query = lcls['query']
+
+        query = query.filter(Article.estimated_price.between(min_price, max_price))
+
+        if search_string:
+            query = query.filter(Article.name.ilike(f"%{search_string}%"))
+
+        return query.count()
+
+    def get_articles_for_sale_min_price(self, types: list, search_string: str):
+        query = self.session.query(Article) \
+            .filter(Article.for_sale == True)
+
+        inner = ", ".join(f"Article.type.has(name='{t}')" for t in types)
+        expr = f"query = query.filter(or_({inner}))"
+
+        lcls = locals()
+        exec(expr, globals(), lcls)
+        query = lcls['query']
+
+        if search_string:
+            query = query.filter(Article.name.ilike(f"%{search_string}%"))
+
+        articles = query.all()
+        return min(articles, key=attrgetter('estimated_price')).estimated_price
+
+    def get_articles_for_sale_max_price(self, types: list, search_string: str):
+        query = self.session.query(Article) \
+            .filter(Article.for_sale == True)
+
+        inner = ", ".join(f"Article.type.has(name='{t}')" for t in types)
+        expr = f"query = query.filter(or_({inner}))"
+
+        lcls = locals()
+        exec(expr, globals(), lcls)
+        query = lcls['query']
+
+        if search_string:
+            query = query.filter(Article.name.ilike(f"%{search_string}%"))
+
+        articles = query.all()
+        return max(articles, key=attrgetter('estimated_price')).estimated_price
 
     def get_article_by_id(self, ar_id):
         return self.session.query(Article).filter_by(article_id=ar_id).first()
